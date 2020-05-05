1a65905e67f7cd40a50f20fc0bed5cee2165a5e1
allisnone <zgx20022002@163.com>
2020-05-05 21:38:59 +0800

    update strategy33

--- zmodels.py
+++ zmodels.py
@@ -9,6 +9,7 @@ from sqlalchemy import Column,Integer,String,Boolean,Float,DateTime
 
 Base = declarative_base()
 class Modversion(Base):
+    """use for mod maintenance"""
     __tablename__ = 'modversion'
     #id = Column(Integer, primary_key=True, autoincrement=True)
     mod = Column(String(32),unique=True)
@@ -26,19 +27,12 @@ class Modversion(Base):
     #columns=['starttime','endtime', 'nexttime', 'status', 'valid','interval','value','count','max','update_time','comment']
     
 
-class Histfund(Base):
-    __tablename__ = 'histfund'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    updatetime = Column(DateTime, default=datetime.datetime.date())
-    market = Column(Float, default=None)
-    capital = Column(Float, default=None)
-    position = Column(Float, default=None)
-    
 class Orderevents(Base):
+    """use for strategory ordering and trading"""
     __tablename__ = 'orderevents'
     id = Column(Integer, primary_key=True, autoincrement=True)
     #orderevent = Column(String(32))#,index=True)
-    updatetime = Column(DateTime, default=datetime.datetime.utcnow)
+    updatetime = Column(DateTime, default=datetime.datetime.now()) #datetime.datetime.utcnow()
     direction = Column(Integer,default=0) #0-buy, 1-sell
     ordertype = Column(Integer,default=0) #0-limit price
     stock = Column(String(6))
@@ -50,14 +44,34 @@ class Orderevents(Base):
     orderid = Column(String(32),default=None)
     tradeid = Column(String(32),default=None)
     starttime = Column(DateTime, default=None)
-    endtime = Column(DateTime,default=None)
+    endtime = Column(DateTime,default=datetime.datetime.strptime(datetime.datetime.date(now).strftime('%Y%m%d')+'145959','%Y%m%d%H%M%S'))
     delay = Column(Integer,default=None)
     interval = Column(Integer,default=None)
     count = Column(Integer,default=None)
     comment = Column(String(32),default='')
 
+class Histfund(Base):
+    """use for recording position and fund"""
+    __tablename__ = 'histfund'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    updatetime = Column(DateTime, default=datetime.datetime.date(now))
+    market = Column(Float, default=None)
+    capital = Column(Float, default=None)
+    position = Column(Float, default=None)
+
+class Histstrategy33(Base):
+    """use for recording Histstrategy33"""
+    __tablename__ = 'histstrategy33'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    updatetime = Column(DateTime, default=datetime.datetime.date(now))
+    stock = Column(String(6))
+    exit = Column(Float, default=None)
+    buy = Column(Float, default=None)
+    stop = Column(Float, default=None) #stop profit, sell in high price, sell then buy in 0-3 dates
+    trying = Column(Float, default=None) ##try to buy, buy in low price, buy then sell in 0-n dates
+    status = Column(Integer) #0-buy, 1-exit,2-stop,3-trying,-1-holding/unchanged
+    success = Column(Boolean,default=False)
      
-columns=['starttime','endtime', 'nexttime', 'status', 'valid','interval','value','count','max','update_time','comment']
 sqlite_db = 'test.db'
 engine = create_engine('sqlite:///' + sqlite_db + '?check_same_thread=False', echo=False)
 Base.metadata.create_all(engine)
