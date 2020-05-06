6396b248e714e603019dbcbbc3faf2720d3de931
allisnone <zgx20022002@163.com>
2020-05-06 09:13:17 +0800

    optimize

--- zmodels.py
+++ zmodels.py
@@ -12,17 +12,17 @@ class Modversion(Base):
     """use for mod maintenance"""
     __tablename__ = 'modversion'
     #id = Column(Integer, primary_key=True, autoincrement=True)
-    mod = Column(String(32),unique=True)
+    mod = Column(String(32),unique=True,primary_key=True)
     version = Column(Integer,default=1)
     updatetime = Column(DateTime, default=datetime.datetime.utcnow)
     endtime = Column(DateTime, default=None)
     nexttime = Column(DateTime, default=None) #next update time
     status = Column(Integer,default=0)
-    valid = Column(Boolean,default=True)
-    interval = Column(Integer,default=None)  #update every interval
+    valid = Column(Boolean,default=True) #1-valid, 0-invalid
+    interval = Column(Integer,default=86400)  #update every interval, default one day
     value = Column(Float,default=None)  #if position, position value
-    count = Column(Integer,default=None)
-    max = Column(Float,default=None)  #if position, the max position rate
+    count = Column(Integer,default=3)
+    max = Column(Float,default=0.8)  #if position, the max position rate
     fixtime =  Column(String(10), default=None) #fixed time to update data every date
     related = Column(String(256),default='') #related stocks 
     comment = Column(String(32),default='')
@@ -40,16 +40,16 @@ class Orderevents(Base):
     stock = Column(String(6))
     price = Column(Float)
     volume = Column(Integer)
-    valid = Column(Boolean,default=True)
-    status = Column(Integer,default=0)
+    valid = Column(Boolean,default=True) #1-valid, 0-invalid
+    status = Column(Integer,default=0) #0-trigger, 1-sent order, 2-traded, 3-revised order, 4-canceled, 5-timeout,6-second-day-order
     strategyid = Column(String(32),default=None)
     orderid = Column(String(32),default=None)
     tradeid = Column(String(32),default=None)
     starttime = Column(DateTime, default=None)
-    endtime = Column(DateTime,default=datetime.datetime.strptime(datetime.datetime.date(now).strftime('%Y%m%d')+'145959','%Y%m%d%H%M%S'))
-    delay = Column(Integer,default=None)
-    interval = Column(Integer,default=None)
-    count = Column(Integer,default=None)
+    endtime = Column(DateTime,default=datetime.datetime.strptime(datetime.datetime.now().strftime('%Y%m%d')+'145959','%Y%m%d%H%M%S'))
+    delay = Column(Integer,default=300)
+    interval = Column(Integer,default=60)
+    count = Column(Integer,default=5)
     comment = Column(String(32),default='')
 
 class Histfund(Base):
@@ -57,7 +57,7 @@ class Histfund(Base):
     __tablename__ = 'histfund'
     id = Column(Integer, primary_key=True, autoincrement=True)
     account = Column(String(32),default='')
-    updatetime = Column(DateTime, default=datetime.datetime.date(now))
+    updatetime = Column(DateTime, default=datetime.datetime.now())
     market = Column(Float, default=0)
     capital = Column(Float, nullable=False)
     cash = Column(Float, default=0)
@@ -67,12 +67,12 @@ class Histstrategy33(Base):
     """use for recording Histstrategy33"""
     __tablename__ = 'histstrategy33'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    updatetime = Column(DateTime, default=datetime.datetime.date(now))
+    updatetime = Column(DateTime, default=datetime.datetime.now())
     stock = Column(String(6))
     exit = Column(Float, nullable=False)
     buy = Column(Float, nullable=False)
-    stop = Column(Float, default=None) #stop profit, sell in high price, sell then buy in 0-3 dates
-    trying = Column(Float, default=None) ##try to buy, buy in low price, buy then sell in 0-n dates
+    stop = Column(Float, default=-1) #stop profit, sell in high price, sell then buy in 0-3 dates
+    trying = Column(Float, default=-1) ##try to buy, buy in low price, buy then sell in 0-n dates
     status = Column(Integer,default=-1) #0-buy, 1-exit,2-stop,3-trying,-1-holding/unchanged
     success = Column(Boolean,default=False)
      
--- zsql_session.py
+++ zsql_session.py
@@ -5,7 +5,7 @@ from zmodels import engine,Orderevents,Modversion
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 db_session = Session()
-
+nt = datetime.datetime.now()
 mod_version = {'positions':[0,nt,None,None,0,1,60,10000.0,3,0.8,'15:01','002256,600345',''],
         'fund':[0,nt,None,None,0,1,60,10000.0,None,None,'15:01','',''],
         'strategy33':[0,nt,None,None,0,1,60,None,3,None,'15:01','',''],
@@ -25,34 +25,55 @@ mod_version = {'positions':[0,nt,None,None,0,1,60,10000.0,3,0.8,'15:01','002256,
         }
 columns=['version','updatetime','endtime', 'nexttime', 'status', 'valid','interval','value','count','max','fixtime','related','comment']
 
-def init_modversion():
+def init_modversion(db_session):
     version = 0
     nt = datetime.datetime.now()
+    #datetime.datetime.strptime(datetime.datetime.now().strftime('%Y%m%d')+'145959','%Y%m%d%H%M%S')
     capital = 10000.0
+    market = 8000.0
     position_stocks = ['300195','300716','600237']
     index_stocks = ['600519','601318']
     starttime = '9:25'
     endtime = '15:01'
     kdatetime = '16:00'
     noontime = '13:10'
-    mods = ['positons','fund','strategy33','hangqing','traderapi','kdate','backtest','report','ipo','tplus','dapan','order','preorder','trade','potential']
+    ttime = '10:30'
+    postclosetime = '14:40'
+    mods = ['positions','fund','strategy33','hangqing','traderapi','kdata','backtest','report','ipo','tplus','dapan','order','preorder','trade','potential']
     for mod in mods:
         related = ''
         value = 0
-        
-        if mod in ['positions','fund','strategy33','tplus','preorder','trade']:
+        fixedtime = None
+        if mod in ['positions','fund','strategy33']:
             related = ','.join(position_stocks)
             value = capital
+            fixedtime = endtime
+        elif mod in ['kdata','backtest','report','potential']:
+            fixedtime = kdatetime
+        elif mod in ['tplus']:
+            fixedtime = ttime
+            value = market
+        elif mod in ['ipo','order','trade']:
+            fixedtime = noontime
+            value = capital - market
+        elif mod in ['preorder','traderapi']:
+            fixedtime = starttime
+            value = capital - market
+        elif mod in ['postorder']:
+            fixedtime = postclosetime
+            value = capital - market
         elif mod=='dapan':
             related = ','.join(index_stocks)
+            fixedtime = kdatetime
         else:
             pass
-        fixedtime = None
-        
         m = Modversion(mod=mod,version=version,updatetime=nt,endtime=None,nexttime=None, status=0, 
                 valid=True,interval=60,value=value,count=3,max=0.8,fixtime=fixedtime,related=related,comment='')
-        
         db_session.add(m)
+    db_session.commit()
+    return
+
+init_modversion(db_session)        
 #add sql data
 order1 = Orderevents(stock='600123',price=6.8,volume=100)
 order2 = Orderevents(stock='600124',price=9.8,volume=200)
@@ -62,9 +83,9 @@ order2 = Orderevents(stock='600124',price=9.8,volume=200)
 db_session.commit()
 
 #update sql data
-event = db_session.query(Orderevents).filter(Orderevents.id >= 3).first()
+event = db_session.query(Orderevents).filter(Orderevents.id >= 2).first()
 print(event.id, event.stock,event.volume)
-res = db_session.query(Orderevents).filter(Orderevents.id==9).update({"volume":400,'valid':False})
+res = db_session.query(Orderevents).filter(Orderevents.id==2).update({"volume":400,'valid':False})
 print(res) # 1 res就是我们当前这句更新语句所更新的行数
 
 db_session.commit()
